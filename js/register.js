3f39a48f074bca4dcfc0726acbb4fc42f5955787
tute sarser <matiassarser735@gmail.com>
2023-06-12 19:03:59 -0300

    register en local storage

--- js/register.js
+++ js/register.js
@@ -1,33 +1,28 @@
 //register
-const inputsRegister = document.getElementById('inputsRegister')
-const div = document.createElement('div')
-div.classList.add()
-div.innerHTML =
-`
+const inputsRegister = document.getElementById('inputsRegister');
+const div = document.createElement('div');
+div.classList.add();
+div.innerHTML = `
 <form id="registrationForm" class="formConfig">
-<input type="text" placeholder= "Nombre de Usuario" class="Usuario" required>
-<input type="email" placeholder= "Email" class="Email" required>
-<input type="date" class="Date" required>
-<input type="password" placeholder= "Contraseña" class="Contraseña" required>
-<input type="submit" value="Registrarse" class="Registrado">
+  <input type="text" placeholder="Nombre de Usuario" class="Usuario" required>
+  <input type="email" placeholder="Email" class="Email" required>
+  <input type="date" class="Date" required>
+  <input type="password" placeholder="Contraseña" class="Contraseña" required>
+  <input type="submit" value="Registrarse" class="Registrado">
 </form>
-
-`
-inputsRegister.appendChild(div)
+`;
+inputsRegister.appendChild(div);
 
 const registrationForm = document.getElementById('registrationForm');
 
-
 registrationForm.addEventListener('submit', function(event) {
-  event.preventDefault(); 
+  event.preventDefault();
 
-  
   const username = document.querySelector('.Usuario').value;
   const email = document.querySelector('.Email').value;
-  const date = document.querySelector('.marginDate').value;
+  const date = document.querySelector('.Date').value;
   const password = document.querySelector('.Contraseña').value;
 
- 
   const userInfo = {
     username: username,
     email: email,
@@ -35,15 +30,16 @@ registrationForm.addEventListener('submit', function(event) {
     password: password
   };
 
- 
   const userInfoJSON = JSON.stringify(userInfo);
 
-  
   localStorage.setItem('userInfo', userInfoJSON);
 
-  
   registrationForm.reset();
 
-  
-  
+ 
 });
+
+
+
+  
+
