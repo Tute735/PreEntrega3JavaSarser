87bd0a757f0fb80ecf5ae218557b1593cebdcf80
tute sarser <matiassarser735@gmail.com>
2023-06-19 14:50:27 -0300

    ...

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const productosArray = [
-    {id: "1", nombre:"Zapatillas Nike Dunk Hi Retro", imagen:"../img/producto1.jpg", precio:58.799, categoria:{marca:"Nike", id:"nike"}},
-    {id: "2", nombre:"Zapatillas Nike Air Force 1 High 07", imagen:'../img/producto2.jpg', precio:64.799, categoria:{marca:"Nike", id:"nike"}},
-    {id: "3", nombre:"Zapatillas adidas Forum Mid",cantidad:1, imagen:"../img/producto3.jpg", precio:37.999, categoria:{marca:"Adidas", id:"adidas"}},
-    {id: "4", nombre:"Zapatillas Jordan 1 Low SE Mujer", imagen:"../img/producto4.jpg", precio:57.999, categoria:{marca:"Nike", id:"nike"}},
-    {id: "5", nombre:"Zapatillas Nike Air Vapormax 2021 Fk", imagen:"../img/producto5.jpg", precio:96.999, categoria:{marca:"Nike", id:"nike"}},
-    {id: "6", nombre:"Zapatillas adidas Forum Mid Thebe ", imagen:"../img/producto6.jpg", precio:57.999, categoria:{marca:"Adidas", id:"adidas"}},
-    {id: "7", nombre:"Zapatillas adidas Superstar Ot Tech", imagen:"../img/producto7.jpg", precio:57.999, categoria:{marca:"Adidas", id:"nike"}},
-    {id: "8", nombre:"Zapatillas Adidas Forum Mid", imagen:"../img/producto8.jpg", precio:55.999, categoria:{marca:"Adidas", id:"nike"}},
-    {id: "9", nombre:"Zapatillas Nike LeBron 19", imagen:"../img/producto9.jpg", precio:79.999, categoria:{marca:"Nike", id:"nike"}},
+    {id: "1", nombre:"Zapatillas Nike Dunk Hi Retro", imagen:"./img/producto1.jpg", precio:58.799, categoria:{marca:"Nike", id:"nike"}},
+    {id: "2", nombre:"Zapatillas Nike Air Force 1 High 07", imagen:'./img/producto2.jpg', precio:64.799, categoria:{marca:"Nike", id:"nike"}},
+    {id: "3", nombre:"Zapatillas adidas Forum Mid",cantidad:1, imagen:"./img/producto3.jpg", precio:37.999, categoria:{marca:"Adidas", id:"adidas"}},
+    {id: "4", nombre:"Zapatillas Jordan 1 Low SE Mujer", imagen:"./img/producto4.jpg", precio:57.999, categoria:{marca:"Nike", id:"nike"}},
+    {id: "5", nombre:"Zapatillas Nike Air Vapormax 2021 Fk", imagen:"./img/producto5.jpg", precio:96.999, categoria:{marca:"Nike", id:"nike"}},
+    {id: "6", nombre:"Zapatillas adidas Forum Mid Thebe ", imagen:"./img/producto6.jpg", precio:57.999, categoria:{marca:"Adidas", id:"adidas"}},
+    {id: "7", nombre:"Zapatillas adidas Superstar Ot Tech", imagen:"./img/producto7.jpg", precio:57.999, categoria:{marca:"Adidas", id:"nike"}},
+    {id: "8", nombre:"Zapatillas Adidas Forum Mid", imagen:"./img/producto8.jpg", precio:55.999, categoria:{marca:"Adidas", id:"nike"}},
+    {id: "9", nombre:"Zapatillas Nike LeBron 19", imagen:"./img/producto9.jpg", precio:79.999, categoria:{marca:"Nike", id:"nike"}},
     
 ]
 let carrito =[]
